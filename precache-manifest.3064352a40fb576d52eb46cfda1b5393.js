9e21c0845ba87f83dd24f2af8c54c7fbb43ea827
gwlrs <gwlrs@users.noreply.github.com>
2022-05-24 15:48:05 +0300

    first

---
+++ precache-manifest.3064352a40fb576d52eb46cfda1b5393.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ba99c84eece1d06e29",
+    "url": "/hello-pwa/css/app.ba014d55.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/hello-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "54d2a54f4c8901bccf831e30c8381383",
+    "url": "/hello-pwa/index.html"
+  },
+  {
+    "revision": "d19aa862653f826a3b22",
+    "url": "/hello-pwa/js/about.6212b351.js"
+  },
+  {
+    "revision": "05ba99c84eece1d06e29",
+    "url": "/hello-pwa/js/app.cd429946.js"
+  },
+  {
+    "revision": "5fdaff654b22e695ebb5",
+    "url": "/hello-pwa/js/chunk-vendors.7ca1af81.js"
+  },
+  {
+    "revision": "29ac9856f70c82dd0a7baab3ae16398c",
+    "url": "/hello-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hello-pwa/robots.txt"
+  }
+]);
